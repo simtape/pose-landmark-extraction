92ca8d348a6ca0466e804bd38ecc3c1690eaf7dc
Simone <scimonscassa@outlook.it>
2024-11-03 23:02:07 +0100

    Code refactor

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ PoseLandmarker = vision.PoseLandmarker
 PoseLandmarkerOptions = vision.PoseLandmarkerOptions
 VisionRunningMode = vision.RunningMode
 
-# Create a pose landmarker instance with the video mode:
 options = PoseLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.VIDEO)
@@ -19,9 +18,7 @@ with PoseLandmarker.create_from_options(options) as landmarker:
     cap = cv2.VideoCapture('lvl_1.mp4')
 
     fps = cap.get(cv2.CAP_PROP_FPS)
-
     frame_number = 0
-    csv_data = []
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -29,13 +26,9 @@ with PoseLandmarker.create_from_options(options) as landmarker:
             break
 
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # calculate the frame timestamp by using the fps
-        # cast to int
         frame_timestamp = int((frame_number / fps) * 1000)
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
 
         pose_landmarker_result = landmarker.detect_for_video(mp_image, frame_timestamp)
         frame_number += 1
-        print(pose_landmarker_result)
