e00b06894ab1ae2c40b0667eabda24351e21fbbc
Simone <scimonscassa@outlook.it>
2024-11-04 10:04:43 +0100

    Add method to write landmarks on frame

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import mediapipe as mp
 from mediapipe import tasks
 from mediapipe.tasks.python import vision
 import cv2
+from visualization_tools import draw_landmarks_on_image
 
 model_path = 'pose_landmarker_full.task'
 
@@ -13,6 +14,8 @@ VisionRunningMode = vision.RunningMode
 options = PoseLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.VIDEO)
+
+mp_drawing = mp.solutions.drawing_utils
 
 with PoseLandmarker.create_from_options(options) as landmarker:
     cap = cv2.VideoCapture('lvl_1.mp4')
@@ -29,6 +32,7 @@ with PoseLandmarker.create_from_options(options) as landmarker:
         frame_timestamp = int((frame_number / fps) * 1000)
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-
         pose_landmarker_result = landmarker.detect_for_video(mp_image, frame_timestamp)
+        print(pose_landmarker_result.pose_landmarks)
+
         frame_number += 1
